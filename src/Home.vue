8ba31734926643946af6277593be347f724f9f3f
Andrés Gz <pippe.111007@gmail.com>
2017-11-20 10:32:13 -0500

    cambiando los links y creando y agregando el nuevo compnente Home

---
+++ src/Home.vue
@@ -0,0 +1,15 @@
+<template>
+    <h2>Esta es la {{texto}} Home</h2>
+</template>
+
+<script>
+export default {
+  name: 'home',
+  data () {
+    return {
+      texto: 'página'
+    }
+  }
+}
+</script>
+
