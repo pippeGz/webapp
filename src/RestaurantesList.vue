40df2e2298e9f801ec609d359bf7d5cd3cdad877
Andrés Gz <pippe.111007@gmail.com>
2017-11-20 16:30:11 -0500

    agregando peticion para traer restaurantes desde la api y configurando lista en el componente restaurantesList

--- src/RestaurantesList.vue
+++ src/RestaurantesList.vue
@@ -1,13 +1,34 @@
 <template>
-    <h2>Esta es la {{texto}}</h2>
+    <div>
+      <ul id="restaurantesL">
+        <li v-for="restaurante in restaurantes">
+          <strong>{{restaurante.title}}</strong>
+          </li> 
+      </ul>
+      <!-- <span v-else>Cargando restaurantes...</span> -->
+    </div>
 </template>
 
 <script>
+import axios from 'axios'
+
 export default {
   name: 'restaurantesL',
+  mounted(){
+    this.getRestaurantes();
+  },
   data () {
     return {
-      texto: 'página Restaurantes'
+      texto: 'página Restaurantes',
+      restaurantes: null
+    }
+  },
+  methods:{
+    getRestaurantes(){
+      axios.get('https://my-json-server.typicode.com/pippeGz/FakeApiRestaurantes/restaurantes')
+        .then((respuesta)=>{
+          this.restaurantes = respuesta.data;
+        });
     }
   }
 }
