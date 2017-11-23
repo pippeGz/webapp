1a8a5fecce8182c110da94793da264fb67f4999c
Andrés Gz <pippe.111007@gmail.com>
2017-11-23 12:46:16 -0500

    agregando logica para guardar un nuevo restaurante en el api

--- src/RestauranteAdd.vue
+++ src/RestauranteAdd.vue
@@ -1,18 +1,18 @@
 <template>
   <div id="restaurante-add">
       <h1>Crear Nuevo Restaurante</h1>
-      <form>
+      <form v-on:submit.prevent="guardarRestaurante()">
           <p>
               <label>Nombre</label>
-              <input type="text">
+              <input type="text" v-model="restaurante.nombre" >
           </p>
           <p>
               <label>Descripcion</label>
-              <input type="text">
+              <input type="text" v-model="restaurante.descripcion" >
           </p>
           <p>
               <label for="">Precio</label>
-              <select>
+              <select v-model="restaurante.precio" >
                   <option value="">Seleccione una opcion...</option>
                   <option value="bajo">Bajo</option>
                   <option value="normal">Normal</option>
@@ -23,3 +23,40 @@
       </form>
   </div>
 </template>
+
+<script>
+import axios from 'axios'
+export default {
+  name: 'restaurante-add',
+  mounted(){
+
+  },
+  data(){
+      return{
+          restaurante:{
+              nombre:'',
+              descripcion:'',
+              precio:'normal'
+          }
+      };
+  },
+  methods:{
+      guardarRestaurante(){
+        console.log(this.restaurante);
+        var router = this.$router;
+        
+        var params = "json="+JSON.stringify(this.restaurante);
+        axios.post('https://my-json-server.typicode.com/pippeGz/FakeApiRestaurantes/restaurantes',params)
+            .then((respuesta)=>{
+                console.log(respuesta);
+                if(respuesta.statusText=='Created'){
+                    router.push('/restaurantes');
+                }
+            })
+            .catch((error)=>{
+                console.log(error);
+            });
+      }
+  }
+}
+</script>
